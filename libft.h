702407626b90a6470cbda1fd0bac52091ecd3f47
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-21 16:43:39 +0100

    .

--- libft.h
+++ libft.h
@@ -1,5 +1,22 @@
-typedef struct		s_list
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 16:40:27 by faventur          #+#    #+#             */
+/*   Updated: 2022/02/21 16:43:16 by faventur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+#endif
