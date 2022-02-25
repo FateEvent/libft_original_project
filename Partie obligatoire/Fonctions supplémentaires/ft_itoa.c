e96cdb8bf28a2b00adde95f8e14be17ab008d63e
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-25 12:36:56 +0100

    itoa

--- Partie obligatoire/Fonctions supplémentaires/ft_itoa.c
+++ Partie obligatoire/Fonctions supplémentaires/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 16:28:54 by faventur          #+#    #+#             */
-/*   Updated: 2022/02/24 19:06:55 by faventur         ###   ########.fr       */
+/*   Updated: 2022/02/25 12:36:43 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,17 @@ static int	len_calculator(int n)
 	int	i;
 
 	i = 0;
-	if (n < 0)
-		n *= -1;
-	if (n == 0)
-		return (1);
-	while (n)
+	if (n == -2147483648)
+		return (11);
+	if (n <= 0)
+		i = 1;
+	while (n != 0)
 	{
+		printf("%d\n", n);
 		n /= 10;
 		i++;
 	}
+	printf("i %d\n", i);
 	return (i);
 }
 
@@ -55,18 +57,20 @@ static char	*ft_rev_str(char *res)
 	return (res);
 }
 
-char	*ft_itoa(int n)
+static char	*ft_itoa(int n)
 {
-	char	*res;
-	int		i;
-	int		x;
+	int			i;
+	long int	x;
+	int			len;
+	char		*res;
 
-	res = (char *)malloc(sizeof(char) * (len_calculator(n) + 1));
 	i = 0;
-	x = n;
+	x = (long) n;
+	len = len_calculator(n);
+	res = (char *)malloc(sizeof(char) * (len + 1));
 	if (n < 0)
 		x = -x;
-	while (i < len_calculator(n))
+	while (i < len_calculator(n) && x != 0)
 	{
 		res[i++] = 48 + (x % 10);
 		x /= 10;
