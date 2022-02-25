d81ee2858c519ffe8e325ce008d73ecb3cade1f6
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-25 20:53:00 +0100

    unsigned_char_comparing

--- Partie obligatoire/Fonctions de la libc/ft_strncmp.c
+++ Partie obligatoire/Fonctions de la libc/ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 18:13:31 by faventur          #+#    #+#             */
-/*   Updated: 2022/02/23 18:13:47 by faventur         ###   ########.fr       */
+/*   Updated: 2022/02/25 20:52:46 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** The strncmp() function is similar to the strcmp() function, except it
-** only compares the first (at most) n bytes of s1 and s2.
+** The strncmp() function is similar to the strcmp() function, except
+** that it only compares the first (at most) n bytes of s1 and s2.
 ** 
 ** Return Value: The strncmp() function returns an integer less than,
 ** equal to, or greater than zero if the first n bytes of s1 are found,
@@ -26,14 +26,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if ((s2[i] == '\0' && s1[i] == '\0') || n == 0)
+	if (n == 0 || (((unsigned char *)s1)[i] != '\0'
+		&& ((unsigned char *)s2)[i] != '\0'))
 		return (0);
 	n--;
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && ((unsigned char *)s1)[i] != '\0'
+		&& ((unsigned char *)s2)[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
