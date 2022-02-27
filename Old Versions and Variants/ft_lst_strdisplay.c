84135f6d0c2b467b7e4381a26c3c3bb1c5dfd574
FateEvent <fabio.venturini.1989@gmail.com>
2022-02-27 20:09:17 +0100

    list_zoom

--- Old Versions and Variants/ft_lst_strdisplay.c
+++ Old Versions and Variants/ft_lst_strdisplay.c
@@ -6,25 +6,11 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 14:45:57 by faventur          #+#    #+#             */
-/*   Updated: 2022/02/27 19:43:34 by faventur         ###   ########.fr       */
+/*   Updated: 2022/02/27 20:08:20 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char const *str)
-{
-	while (*str != '\0')
-	{
-		ft_putchar(*str);
-		str++;
-	}
-}
+#include "libft.h"
 
 void	ft_lst_strdisplay(t_list *first)
 {
@@ -35,8 +21,7 @@ void	ft_lst_strdisplay(t_list *first)
 		return ;
 	while (current != NULL)
 	{
-		ft_putstr(current->content);
-		ft_putchar('\n');
+		ft_putendl(current->content);
 		current = current->next;
 	}
 	ft_putstr("NULL\n");
