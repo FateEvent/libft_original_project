7e016008fe4ae0a29035a59e298c96c0d6a23967
FateEvent <fabio.venturini.1989@gmail.com>
2022-03-02 12:17:07 +0100

    ça a l'air parfait

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 09:53:37 by faventur          #+#    #+#             */
-/*   Updated: 2022/02/28 16:53:06 by faventur         ###   ########.fr       */
+/*   Updated: 2022/03/02 12:16:25 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (!*lst || !del)
+	if (!lst || !*lst || !del)
 		return ;
 	while (*lst != NULL)
 	{
